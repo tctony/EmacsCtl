fbf9de4789ff8b5114753c1f3806af4a9d70e78a
Tony Tang <tangchang21@gmail.com>
2023-12-07 23:31:51 +0800

    launch as agent

--- EmacsCtl/AppDelegate.swift
+++ EmacsCtl/AppDelegate.swift
@@ -10,11 +10,9 @@ import Cocoa
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    
-
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        print("did launch as agent")
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
