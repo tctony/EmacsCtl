a0da5e08689db450181d0bd12716b92947dffa8d
Tony Tang <tangchang21@gmail.com>
2023-12-07 23:27:15 +0800

    create project

---
+++ EmacsCtl/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  EmacsCtl
+//
+//  Created by Tony Tang on 2023/12/7.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
