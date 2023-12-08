6e496b963ce96356e1782865175293987d2e01b3
Tony Tang <tangchang21@gmail.com>
2023-12-08 10:59:51 +0800

    add app icon and tray icon

--- EmacsCtl/AppDelegate.swift
+++ EmacsCtl/AppDelegate.swift
@@ -10,9 +10,15 @@ import Cocoa
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
+    let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.squareLength)
+
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
         print("did launch as agent")
+
+        if let button = statusItem.button {
+            button.image = NSImage(named:NSImage.Name("tray"))
+        }
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
